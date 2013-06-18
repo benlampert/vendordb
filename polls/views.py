2c3ef0e4672f611453d668d7e01fd3f56c29bf6e
blamps <benlampert@gmail.com>
2013-06-17 22:14:21 -0400

    adding all back

--- polls/views.py
+++ polls/views.py
@@ -1,20 +1,38 @@
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-# Create your views here.
-from django.http import HttpResponse
-from polls.models import Poll
+from django.views import generic
+from django.shortcuts import render, get_object_or_404
 
-def index(request):
-	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-	output = ', '.join([p.question for p in latest_poll_list])
-	return HttpResponse(output)
+from polls.models import Poll, Choice
 
-def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." % poll_id)
+class IndexView(generic.ListView):
+	template_name = 'polls/index.html'
+	context_object_name = 'latest_poll_list'
 
-def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	def get_queryset(self):
+		return Poll.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+	model = Poll
+	template_name = 'polls/results.html'
 
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." % poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:	
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render(reqeust, 'polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice.",
+			})
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 
--- vdb/urls.py
+++ vdb/urls.py
@@ -14,7 +14,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-	url(r'^polls/', include('polls.urls')),
+	url(r'^polls/', include('polls.urls', namespace="polls")),
 	url(r'^admin/', include(admin.site.urls)),
 
 )
