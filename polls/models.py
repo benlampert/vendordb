b8f05bc31c99ab5be0431f88fa4f2cdd46fdbc45
Ben Lampert <benlampert@gmail.com>
2013-06-21 23:14:09 -0400

    changed models to vendor

--- polls/models.py
+++ polls/models.py
@@ -4,21 +4,29 @@ import datetime
 
 # Create your models here
 
-class Poll(models.Model):
-	question = models.CharField(max_length=200)
+class Vendor(models.Model):
+	name = models.CharField(max_length=200)
 	pub_date = models.DateTimeField('date_published')
-	def __unicode__(self):
-		return self.question
-	def was_published_recently(self):
+    
+    def __unicode__(self):
+		return self.name
+	
+    def was_published_recently(self):
 		return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 		was_published_recently.admin_order_field = 'pub_date'
 		was_published_recently.boolean = True
 		was_published_recently.short_description = 'Published recently'
  
-class Choice(models.Model):
-	poll = models.ForeignKey(Poll)
-	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
+class VendorDetails(models.Model):
+	vendor = models.ForeignKey(Vendor)
+	description = models.CharField(max_length=1000)
+	internalPOV = models.CharField(max_length=1000)
+    externalPOV = models.charField(max_length=1000)
 	def __unicode__(self):
-		return self.choice_text
+		return self.description
+
+class VendorVisits(models.Model):
+    vendor = models.ForeignKey(Vendor)
+    meetingDate = models.DateTimeField('meeting_date')
+    
 
